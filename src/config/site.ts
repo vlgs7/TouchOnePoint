29d11103cc59f5254c43f42ee26323be89f481d1
vlgs7 <yapengyu77@gmail.com>
2023-08-12 17:26:25 +0800

    og:image url

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: 'TouchOnePonint',
   url: '',
-  ogImage: '/og.png',
+  ogImage: 'https://touch-one-point.vercel.app/og.png',
   description:
     'With TouchOnePoint, bring your personal links, profiles, and digital presence into one cohesive platform. Simple to use. Flexible to customize. All about you.',
   links: {
